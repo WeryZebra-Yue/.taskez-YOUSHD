31b64bf0933544b8d2e3a671ca0d6664abe8fe7f
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-10-31 23:09:22 +0530

    [Refactor]: Use ES6 async/await instead of callback

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -3,21 +3,19 @@ import { getDatabase, onValue, ref, set } from "firebase/database";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 // export
-export const LogIn = (email, password) => {
+export const LogIn = async (email, password) => {
   const database = getDatabase();
   const auth = getAuth();
 
-  // login with email and password
-  signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      return [true, user];
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      console.log(errorMessage, errorCode);
-      return [false, errorMessage];
-    });
+  try {
+    // login with email and password
+    const userCredential = await signInWithEmailAndPassword(email, password);
+    const user = userCredential.user;
+    return [true, user];
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage, errorCode);
+    return [false, errorMessage];
+  }
 };
